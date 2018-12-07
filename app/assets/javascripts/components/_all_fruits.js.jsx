7bb9c7092bf4ec5c641811f89157b8921130a27b
ilyana19 <aries_2152@hotmail.com>
2018-12-07 15:25:38 -0500

    finished edit functions

--- app/assets/javascripts/components/_all_fruits.js.jsx
+++ app/assets/javascripts/components/_all_fruits.js.jsx
@@ -2,7 +2,7 @@ const AllFruits = (props) => {
   const fruits = props.fruits.map((fruit) => {
     return (
       <div key={fruit.id}>
-        <Fruit fruit={fruit} handleDelete={props.handleDelete} />
+        <Fruit fruit={fruit} handleDelete={props.handleDelete} handleUpdate={props.handleUpdate} />
       </div>
     )
   })
